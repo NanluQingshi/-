2da75247ee7402d1d102afe4ea555b125e98ebcb
nlqs <nlqs@nlqs.com>
2024-04-04 22:22:28 +0800

    02-ElementPlus导入及相关配置

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,42 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+// 或者  unplugin-element-plus =======================
+// import ElementPlus from 'unplugin-element-plus/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    // 配置插件
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [
+        // 1.配置 ElementPlus 采用 sass 样式配色系统
+        ElementPlusResolver({ importStyle: "sass" }),
+      ],
+    }),
+    // 或者  unplugin-element-plus ===================== 按需定制主题配置
+    // ElementPlus({
+    //   useSource: true,
+    // }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/element/index.scss" as *;`,
+      },
+    },
   }
 })
