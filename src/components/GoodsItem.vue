ecfba494a05c62bd606ab2e72203f58954f87a66
NanluQingshi <yimo@mail.ustc.edu.cn>
2024-04-09 16:11:24 +0800

    30-商品详情页_页面搭建&路由配置

--- src/components/GoodsItem.vue
+++ src/components/GoodsItem.vue
@@ -14,7 +14,7 @@ defineProps({
 
 <template>
   <div>
-    <RouterLink to="/" class="goods-item">
+    <RouterLink :to="`/detail/${goods.id}`" class="goods-item">
       <img v-img-lazy="goods.picture" alt="" />
       <p class="name ellipsis">{{ goods.name }}</p>
       <p class="desc ellipsis">{{ goods.desc }}</p>
