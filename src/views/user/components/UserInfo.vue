b7da018fc61629e06820134e7e3b824ad158dd72
nlqs <nlqs@nlqs.com>
2024-04-14 15:37:40 +0800

    54-用户中心页面基础结构_三级路由

---
+++ src/views/user/components/UserInfo.vue
@@ -0,0 +1,14 @@
+<!--
+ * @Author: nlqs
+ * @Date: 2024-04-14 15:20:11
+ * @Description: 用户信息
+-->
+<script setup>
+</script>
+
+<template>
+  <div>用户信息</div>
+</template>
+
+<style scoped>
+</style>
